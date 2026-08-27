c1aa054df123804afff9e12495a8614a2f85f32a
Oskar Eichler <oskar@songstats.com>
2026-08-28 02:52:12 +0300

    Retry file acquisition after an atomic replacement

--- lib/pstore.rb
+++ lib/pstore.rb
@@ -621,22 +621,27 @@ class PStore
   # All exceptions are propagated.
   #
   def open_and_lock_file(filename, read_only)
-    if read_only
-      begin
-        file = File.new(filename, **RD_ACCESS)
-      rescue Errno::ENOENT
-        return nil
+    filename = File.path(filename)
+    loop do
+      if read_only
+        begin
+          file = File.new(filename, **RD_ACCESS)
+        rescue Errno::ENOENT
+          return nil
+        end
+      else
+        file = File.new(filename, **RDWR_ACCESS)
       end
-    else
-      file = File.new(filename, **RDWR_ACCESS)
+      current = false
+      begin
+        file.flock(read_only ? File::LOCK_SH : File::LOCK_EX)
+        # An atomic save may have replaced the file while this lock was pending.
+        current = File.identical?(file, filename)
+        return file if current
+      ensure
+        file.close unless current
+      end
     end
-    begin
-      file.flock(read_only ? File::LOCK_SH : File::LOCK_EX)
-    rescue Exception
-      file.close
-      raise
-    end
-    file
   end
 
   # Load the given PStore file.
